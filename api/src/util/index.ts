c8aa7d01af663e5fe0bd837e389133d80670099c
Bill Kerr <48515765+bk7987@users.noreply.github.com>
2020-06-15 09:36:00 -0400

    Minor cleanup

--- api/src/util/index.ts
+++ api/src/util/index.ts
@@ -2,7 +2,6 @@ import { keyExists } from './key-exists';
 import { getUTCDateFromString, parseUTCDateList } from './dates';
 
 export {
-  keyExists,
   getUTCDateFromString,
   parseUTCDateList
 };
--- api/src/util/key-exists.ts
+++
@@ -1,3 +0,0 @@
-export const keyExists = (obj: {}, key: string): boolean => {
-  return Object.keys(obj).includes(key);
-};
