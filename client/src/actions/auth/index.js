ab9096ae89016f54119bd026034246164758cdb2
Bill Kerr <48515765+bk7987@users.noreply.github.com>
2020-10-30 07:44:46 -0400

    Removed old client app

--- client/src/actions/auth/index.js
+++
@@ -1,7 +0,0 @@
-import {
-  SIGN_IN
-} from '../types';
-
-export const signIn = user => {
-  return { type: SIGN_IN, payload: user };
-};
--- client/src/actions/settings/index.js
+++
@@ -1,7 +0,0 @@
-import {
-  SET_CURRENCY_CODE
-} from '../types';
-
-export const setCurrencyCode = currencyCode => {
-  return { type: SET_CURRENCY_CODE, payload: currencyCode };
-};
